3662a73a9b9dcd3e80ea3107fdb2e67cf74aa73c
Brenton Williams <brentw263@gmail.com>
2017-03-26 17:53:32 -0500

    Update battleship.py
    Adapted code structure to fit Python OOP standards

--- battleship.py
+++ battleship.py
@@ -1,47 +1,76 @@
 from random import randint
 
-board = []
+class Battleship:
+    global board  #Create an empty array to represent game board
+    board = []
 
-for x in range(5):
-    board.append(["O"] * 5)
+    def new_board(self, board):           #Creates a new 5 x 5 board with "O"'s for each position on the board
+        turn = 0
+        for x in range(5):
+            board.append(["O"] * 5)
+        return board
 
-def print_board(board):
-    for row in board:
-        print " ".join(row)
+    def print_board(self, board):         #Custom print to organize game board
+        for row in board:
+            print " ".join(row)
 
-print "Let's play Battleship!"
-print_board(board)
+    def random_row(self, board):  #picks a random row for the battleship
+        return randint(0, len(board) - 1)
 
-#AI function goes here
+    def random_col(self, board):          #picks a random col for the battleship
+        return randint(0, len(board[0]) - 1)
+    
 
-def random_row(board):
-    return randint(0, len(board) - 1)
+    def new_game(self, board):            #Creates a new board, resets guess attempts
+        self.board = []
+        self.new_board(board)
 
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
+        ship_row = self.random_row(board)
+        ship_col = self.random_col(board)
 
 
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
-for turn in range(4):
-    guess_row = input("Guess Row:")
-    guess_col = input("Guess Col:")
+        print "Let's play Battleship!"
+        self.print_board(board)
 
-    if guess_row == ship_row and guess_col == ship_col:
-        print "You sunk my battleship!"
-        break
-    else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
-        elif (board[guess_row][guess_col] == "X"):
-            print "You guessed that one already."
+        for turn in range(4):       
+            guess_row = input("Guess Row:")
+            guess_col = input("Guess Col:")
+
+            if guess_row == ship_row and guess_col == ship_col:
+                print "Congratulations! You sunk my battleship!"
+                break
+            else:
+                if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+                    print "Oops, that's not even in the ocean."
+                elif (board[guess_row][guess_col] == "X"):
+                    print "You guessed that one already."
+                else:
+                    print "You missed my battleship!"
+                    board[guess_row][guess_col] = "X"
+            print (turn+1)          #Display how many turns have been used
+            self.print_board(board)
+
+        if (turn + 1) == 4:
+            print "Game Over"
+
+    def play_again(self):
+        userInput = raw_input("Would you like to play again? Enter y/n :")
+        if userInput == 'n':
+            raw_input("Press Enter to exit")
+        elif userInput == 'y':
+            self.new_game(self.board)
         else:
-            print "You missed my battleship!"
-            board[guess_row][guess_col] = "X"
-    print (turn+1)    # Print (turn + 1) here!
-    print_board(board)
-if (turn + 1) == 4:
-    print "Game Over"
+            print "Invalid selection"
+            self.playAgain()
+
+
+def main():
+
+    x = Battleship()
+
+    x.new_game(board)
+
+    x.play_again()
+
+if __name__ == "__main__":
+    main()
